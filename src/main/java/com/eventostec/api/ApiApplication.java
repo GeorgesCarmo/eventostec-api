a3738294eac84db728a883eb0a966b344daff12b
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-07-04 10:06:37 -0300

    working on code

--- src/main/java/com/eventostec/api/ApiApplication.java
+++ src/main/java/com/eventostec/api/ApiApplication.java
@@ -2,7 +2,9 @@ package com.eventostec.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan(basePackages = "com.amazonaws.services.s3.AmazonS3")//("com.amazonaws.services.s3.AmazonS3")
 @SpringBootApplication
 public class ApiApplication {
 
