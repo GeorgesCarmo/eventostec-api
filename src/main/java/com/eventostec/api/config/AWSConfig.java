51c747cce7ec871c2f0cefa4e153ad7337a3ca43
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-07-09 15:41:09 -0300

    missed @Bean

--- src/main/java/com/eventostec/api/config/AWSConfig.java
+++ src/main/java/com/eventostec/api/config/AWSConfig.java
@@ -1,10 +1,16 @@
 package com.eventostec.api.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
 
 @Configuration
 public class AWSConfig {
@@ -12,6 +18,7 @@ public class AWSConfig {
     @Value("{aws.region}")
     private String awsRegion;
 
+    @Bean
     public AmazonS3 createS3Instance(){
         return AmazonS3ClientBuilder
                 .standard()
