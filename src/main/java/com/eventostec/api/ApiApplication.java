cbaa6e44ea1ef18e4273808c01031bbaedad9083
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-07-09 15:41:35 -0300

    no change

--- src/main/java/com/eventostec/api/ApiApplication.java
+++ src/main/java/com/eventostec/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.eventostec.api;
 
+import com.eventostec.api.service.EventService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,5 +12,4 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
